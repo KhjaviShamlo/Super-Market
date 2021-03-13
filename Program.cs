8c7de7a61ac91cc085a0cc11c9484383a96fec9f
ali Hoseyn Khajavi <asia.rayaneh.65@gmail.com>
2021-03-13 13:02:01 +0330

    Create Entites

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Super_Market
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
