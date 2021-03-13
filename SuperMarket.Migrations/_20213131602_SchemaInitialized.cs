96827a55672684fcbe6d998b48f612a12dd23242
ali Hoseyn Khajavi <asia.rayaneh.65@gmail.com>
2021-03-13 16:36:32 +0330

    create EFDataContext

---
+++ SuperMarket.Migrations/_20213131602_SchemaInitialized.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMarket.Migrations
+{
+    class _20213131602_SchemaInitialized
+    {
+    }
+}
---
+++ SuperMarket.Persistence.EF/EFDataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperMarket.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SuperMarket.Persistence.EF
+{
+    public class EFDataContext : DbContext
+    {
+        public DbSet<Good> Goods { get; set; }
+        public DbSet<GoodCategory> GoodCategores { get; set; }
+        public DbSet<GoodEntry> GoodEntries { get; set; }
+        public DbSet<SalesFactor> SalesFactors { get; set; }
+        public EFDataContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
