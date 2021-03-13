29fd38c55f58bfee5669647812f30adff6f6029d
ali Hoseyn Khajavi <asia.rayaneh.65@gmail.com>
2021-03-13 15:28:59 +0330

    create Domain folder and Salesfactor Map

--- SuperMarket.Persistence.EF/GoodEntryMap/GoodEntryEntityMap.cs
+++ SuperMarket.Persistence.EF/GoodEntryMap/GoodEntryEntityMap.cs
@@ -1,10 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entites;
 namespace SuperMarket.Persistence.EF.GoodEntryMap
 {
-    class GoodEntryEntityMap
+    public class GoodEntryEntityMap : IEntityTypeConfiguration<GoodEntry>
     {
+        public void Configure(EntityTypeBuilder<GoodEntry> _)
+        {
+            _.ToTable("GoodEntries");
+            _.HasKey(_ => _.Id);
+            _.Property(_ => _.Id).ValueGeneratedOnAdd();
+            _.Property(_ => _.GoodCode).IsRequired().HasMaxLength(10);
+            _.Property(_ => _.GoodCount).IsRequired();
+            _.Property(_ => _.EntryDate).IsRequired();
+        }
     }
 }
---
+++ SuperMarket.Persistence.EF/SalesFactorMap/SalesFactorEntityMap.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entites;
+
+namespace SuperMarket.Persistence.EF.SalesFactorMap
+{
+    public class SalesFactorEntityMap : IEntityTypeConfiguration<SalesFactor>
+    {
+        public void Configure(EntityTypeBuilder<SalesFactor> _)
+        {
+            _.ToTable("SalesFactors");
+            _.HasKey(_ => _.Id);
+            _.Property(_ => _.Id).IsRequired().ValueGeneratedOnAdd();
+            _.Property(_ => _.GoodCode).IsRequired().HasMaxLength(10);
+            _.Property(_ => _.GoodCount).IsRequired();
+            _.Property(_ => _.SalesDate).IsRequired();
+        }
+    }
+}
