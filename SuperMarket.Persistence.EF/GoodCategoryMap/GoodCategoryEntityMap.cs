c68b7137a6b19766ea4ee752a838b3f7f5fd6e02
ali Hoseyn Khajavi <asia.rayaneh.65@gmail.com>
2021-03-13 14:13:53 +0330

    Create GoodCtegoryMap And GoodsMap

---
+++ SuperMarket.Persistence.EF/GoodCategoryMap/GoodCategoryEntityMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entites;
+namespace SuperMarket.Persistence.EF.GoodCategoryMap
+{
+    class GoodCategoryEntityMap : IEntityTypeConfiguration<GoodCategory>
+    {
+        public void Configure(EntityTypeBuilder<GoodCategory> _)
+        {
+            _.ToTable("GoodCategores");
+            _.HasKey(_ => _.Id);
+            _.Property(_ => _.Id).IsRequired().ValueGeneratedOnAdd();
+            _.Property(_ => _.Title).IsRequired();
+            _.HasMany(_ => _.Goods).WithOne(_ => _.Category).HasForeignKey(_ => _.CategoryId).OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ SuperMarket.Persistence.EF/GoodsMap/GoodEntityMap.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SuperMarket.Entites;
+namespace SuperMarket.Persistence.EF.GoodsMap
+{
+    public class GoodEntityMap : IEntityTypeConfiguration<Good>
+    {
+        public void Configure(EntityTypeBuilder<Good> _)
+        {
+            _.ToTable("Goods");
+            _.HasKey(_ => _.Id);
+            _.Property(_ => _.Id).IsRequired().ValueGeneratedOnAdd();
+            _.Property(_ => _.Code).IsRequired().HasMaxLength(10);
+            _.Property(_ => _.Count).IsRequired();
+            _.Property(_ => _.Title).IsRequired().HasMaxLength(50);
+        }
+    }
+}
