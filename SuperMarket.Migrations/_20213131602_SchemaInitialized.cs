f9393ff4acc6dacf19e11e6b71d1f377159b9885
ali Hoseyn Khajavi <asia.rayaneh.65@gmail.com>
2021-03-14 15:55:23 +0330

    Edit Migration

--- SuperMarket.Migrations/_20213131602_SchemaInitialized.cs
+++ SuperMarket.Migrations/_20213131602_SchemaInitialized.cs
@@ -9,25 +9,32 @@ namespace SuperMarket.Migrations
     {
         public override void Down()
         {
-            Create.Table("Goods").WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey()
-                .WithColumn("Code").AsString(10).NotNullable()
-                .WithColumn("Title").AsString(50).NotNullable()
-                .WithColumn("Count").AsInt32();
-            Create.Table("GoodCategores").WithColumn("Id").AsInt32().PrimaryKey()
-                .WithColumn("Title").AsString().Identity().NotNullable();
-            Create.Table("GoodEntries").WithColumn("Id").AsString().Identity().PrimaryKey()
-                .WithColumn("GoodCode").AsString().NotNullable()
-                .WithColumn("GoodCount").AsInt32()
-                .WithColumn("EntryDate").AsDateTime();
-            Create.Table("SalesFactors").WithColumn("Id").AsInt32().NotNullable().PrimaryKey()
-                .WithColumn("GoodCode").AsString(10).NotNullable()
-                .WithColumn("GoodCount").AsInt32()
-                .WithColumn("SalesDate").AsDateTime();
         }
 
         public override void Up()
         {
-            throw new NotImplementedException();
+            Create.Table("GoodCategores")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity().NotNullable()
+                .WithColumn("Title").AsString().Identity().NotNullable();
+
+            Create.Table("Goods")
+                .WithColumn("Id").AsInt32().Identity().NotNullable().PrimaryKey()
+                .WithColumn("Code").AsString(10).NotNullable()
+                .WithColumn("Title").AsString(50).NotNullable()
+                .WithColumn("Count").AsInt32()
+                .WithColumn("CategoryId").AsInt32().ForeignKey("FK_Goods_GoodsCategores", "GoodCategores", "Id");
+
+            Create.Table("GoodEntries")
+                .WithColumn("Id").AsString().Identity().PrimaryKey().NotNullable()
+                .WithColumn("GoodCode").AsString().NotNullable()
+                .WithColumn("GoodCount").AsInt32()
+                .WithColumn("EntryDate").AsDateTime();
+
+            Create.Table("SalesFactors")
+                .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().NotNullable()
+                .WithColumn("GoodCode").AsString(10).NotNullable()
+                .WithColumn("GoodCount").AsInt32()
+                .WithColumn("SalesDate").AsDateTime();
         }
     }
 }
